22ca6402350672601e5a4652698b36e0f0bb57eb
Linas <linus.supremus@gmail.com>
2021-03-15 13:23:49 +0200

    commit

--- 07-for.js
+++ 07-for.js
@@ -27,10 +27,29 @@ for(let i = 1; i <= 5; i++) {
 
     console.log('----------');
 
-    const marks = [5, 7, 10, 4, 2, 10];
+    const marks = [5, 7, 10, 4, 2, 10, 8, 9, 1, 3 ];
 
-
-    for (let i = 0; i < 7; i++) {
-        console.log(marks[i]);
-    }
+ for (let i = 0; i < 7; i++) {
+        console.log(marks[i]); }
 }
+
+
+console.log('----------');
+
+const zmones = [5, 7, 10, 4, 2, 10, 8, 9, 1, 3];
+let sum = 0;
+
+for (let i = 0; < marks.lengtg; i++) {
+    const pazymys = marks[i];
+    sum += pazymys;
+}
+
+const average = sum / marks.length;
+
+console.log(sum, '/', marks.length, '=', average);
+
+console.log('---------');
+
+const sakinys = 'Labas Vakaras';
+
+for (let i=0; i < sakinys.length; i++) 
