756a870d7c9695c4e28500c912269fbf330634da
Linas <linus.supremus@gmail.com>
2021-03-15 13:19:00 +0200

    engage

---
+++ 07-for.js
@@ -0,0 +1,36 @@
+
+
+
+
+
+
+
+const abc = ['r', 'y', 't', 'a', 's'];
+
+let zodis = '';
+let abcIndex = 0; 
+
+zodis += abc[abcindex++];
+zodis += abc[abcindex++];
+zodis += abc[abcindex++];
+zodis += abc[abcindex++];
+zodis += abc[abcindex++];
+
+console.log(zodis);
+
+
+for(let i = 1; i <= 5; i++) {
+    console.log(i);
+
+
+    console.log('ciklo pabaiga');
+
+    console.log('----------');
+
+    const marks = [5, 7, 10, 4, 2, 10];
+
+
+    for (let i = 0; i < 7; i++) {
+        console.log(marks[i]);
+    }
+}
